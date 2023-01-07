96f3400c2867899a88d3d3fe135a74b66071eda3
Carson B <hyperdriveguy@gmail.com>
2023-01-07 01:23:35 +0000

    Simplify program using `inc` instruction
    I literally designed the instruction set and forgot I made this an instruction lol

--- hello_world.asm
+++ hello_world.asm
@@ -3,14 +3,13 @@ const counter_stop, 3
 
 Start:
     ld r1, 0
-    ld r2, 1
-    ld r3, counter_stop
+    ld r2, counter_stop
 Loop:
     disp_str "Hello World!\n"
     ; Increment counter by 1
-    add r1, r1, r2
+    inc r1
     ; Check if counter reached a value of 3
-    cp r1, r3
+    cp r1, r2
     jpnc Loop
     ; Fall through to program end
     disp_str "End program.\n"
